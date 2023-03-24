7b81c10a3ebd442bdb3fc6daa70149a15072561e
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2023-03-24 18:02:11 +0100

    Position object

---
+++ src/Gomoku/Structures/Position.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku.Structures
+{
+    public struct Position
+    {
+        public int x;
+        public int y;
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Position(string s)
+        {
+            try
+            {
+                string[] ss = s.Trim().Split(new char[] { ' ', ';', ',', ':' }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                x = int.Parse(ss[0]);
+                y = int.Parse(ss[1]);
+            }
+            catch
+            {
+                throw new Exception("String couldn't be parsed to a position, format is : x y");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"column {x}, row {y}";
+        }
+
+    }
+}
