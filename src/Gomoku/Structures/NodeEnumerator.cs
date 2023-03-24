d71ecb2c15b26caf5cabd91577ed79042e38c307
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2023-03-24 18:03:38 +0100

    NodeEnumerator for AccessLinkedList

---
+++ src/Gomoku/Structures/NodeEnumerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku.Structures
+{
+    /// <summary>
+    /// An enumerator for every index of existing nodes in an AccessLinkedList
+    /// </summary>
+    public class NodeEnumerator<T> : IEnumerator<int>
+    {
+
+        public int Current { get; protected set; }
+        public int Next { get; protected set; }
+        public T CurrentValue { get
+            {
+                return List.values[Current];
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        private AccessLinkedList<T> List;
+
+        public NodeEnumerator(AccessLinkedList<T> list)
+        {
+            List = list;
+            Reset();
+        }
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            if (Current == List.Last)
+                return false;
+            Current = Next;
+            Next = List.nodes[Current].y;
+            return true;
+        }
+
+        public void Reset()
+        {
+            Next = List.First;
+            Current = -1;
+        }
+    }
+}
