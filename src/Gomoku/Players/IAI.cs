99930311e8141ed99bd110f9f77f1b1a76f3f62e
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2023-03-24 18:01:32 +0100

    Interfaces

---
+++ src/Gomoku/Players/IAI.cs
@@ -0,0 +1,9 @@
+﻿using Gomoku.Structures;
+using Gomoku.Players;
+
+namespace Gomoku.Players
+{
+    public interface IAI : IPlayer
+    {
+    }
+}
---
+++ src/Gomoku/Players/IPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gomoku.Structures;
+using Gomoku.Gomoku;
+
+namespace Gomoku.Players
+{
+    public interface IPlayer
+    {
+        public Gomoku.Gomoku Game { get; }
+        public byte Player { get; }
+        public Position Play();
+        public void AlertPlay(Position pos, IPlayer player);
+        public string Name { get; }
+    }
+}
