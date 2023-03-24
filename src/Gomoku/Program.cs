4b1bfc39f1b7fe90b035b849cd19abf86eb562ef
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2023-03-24 18:04:28 +0100

    Main entry

---
+++ src/Gomoku/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gomoku.Gomoku;
+using Gomoku.Structures;
+using System.Diagnostics;
+using Gomoku.Competition;
+using Gomoku.Players;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        TicTacToe game = new TicTacToe();
+        Simulator s = new Simulator(
+            game,
+            new IPlayer[2] {
+                //new ConsolePlayer(game, 0, "AAAA"),
+                new TTTAI(game, 0),
+                new TTTAI(game, 1)
+            },
+            true
+        );
+        s.RunConsole();
+    }
+
+}
